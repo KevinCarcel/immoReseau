8c56bc4fed23c912d63174873855fa3476283d68
KevinCarcel <kevcarcel@yahoo.fr>
2023-06-24 11:40:37 +0200

    maj finale burger menu

--- javascript/global.js
+++
@@ -1,4 +0,0 @@
-function toggleMenu2() {
-  var navigation = document.querySelector('.nav-mobile');
-  navigation.computedStyleMap.display = (navigation.computedStyleMap.display === 'block') ? 'none' : 'block';
-}
